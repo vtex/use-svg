af3bb58b3635af9ab7210a4ea819e96fefc0ccdc
Matheus P. Silva <matheus.ps@icloud.com>
2018-11-09 15:55:29 -0300

    Fix identation with 4 spaces instead of 2

--- react/UseSvg.js
+++ react/UseSvg.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Use = ({id, ...props}) => (
-    <use href={`#${id}`} xlinkHref={`#${id}`} {...props} />
+  <use href={`#${id}`} xlinkHref={`#${id}`} {...props} />
 );
 
 export default Use;
