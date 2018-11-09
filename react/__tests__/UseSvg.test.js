1f88b1f3a392f3e9e3b91efb5b24f64bb8a853f5
Matheus P. Silva <matheus.ps@icloud.com>
2018-11-09 15:54:11 -0300

    Fix identation with 4 spaces instead of 2

--- react/__tests__/UseSvg.test.js
+++ react/__tests__/UseSvg.test.js
@@ -3,30 +3,29 @@ import UseSvg from '../UseSvg';
 import { shallow } from 'enzyme';
 
 describe('ReactUseSvg', () => {
-  
-    let wrapper;
+  let wrapper;
 
-    beforeEach(() => {
-        wrapper = shallow( <UseSvg id="id" x="20" y="50" /> );
-    });
+  beforeEach(() => {
+    wrapper = shallow( <UseSvg id="id" x="20" y="50" /> );
+  });
 
-    it('should render its children', () => {
-        // Act
-        const result = wrapper.find('use');
+  it('should render its children', () => {
+    // Act
+    const result = wrapper.find('use');
         
-        // Assert
-        expect(result).toHaveLength(1);
-    });
+    // Assert
+    expect(result).toHaveLength(1);
+  });
 
-    it('should pass down the data in the correct structure', () => {
-        // Act
-        const result = wrapper.find('use').props();
+  it('should pass down the data in the correct structure', () => {
+    // Act
+    const result = wrapper.find('use').props();
     
-        // Assert
-        expect(result.href).toBeTruthy();
-        expect(result.xlinkHref).toBeTruthy();
-        expect(result.x).toBeTruthy();
-        expect(result.y).toBeTruthy();
-    });
+    // Assert
+    expect(result.href).toBeTruthy();
+    expect(result.xlinkHref).toBeTruthy();
+    expect(result.x).toBeTruthy();
+    expect(result.y).toBeTruthy();
+  });
     
 })
